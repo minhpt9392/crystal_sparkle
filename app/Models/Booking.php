994e18ba4e717bd455c8636779d60b2a192f93b1
Minhpt <minhpt@crystal-techs.com>
2017-09-24 20:21:59 +0700

    Push booking table

---
+++ app/Models/Booking.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ThanhMinh92it
+ * Date: 9/24/2017
+ * Time: 8:09 PM
+ */
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Booking extends Model
+{
+    public $timestamps = false;
+}
---
+++ database/migrations/2017_09_24_131427_create_bookings_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('customer_id')->nullable();
+            $table->integer('shop_id')->nullable();
+            $table->integer('package_id')->nullable();
+            $table->date('date_start')->nullable();
+            $table->integer('therapist_id')->nullable();
+            $table->integer('time')->nullable();
+            $table->string('time_start')->nullable();
+            $table->string('promotion')->nullable();
+            $table->string('create_by')->nullable();
+            $table->string('update_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('bookings');
+    }
+}
