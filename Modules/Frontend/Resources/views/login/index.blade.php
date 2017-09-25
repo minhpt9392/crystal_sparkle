b36e923d3fd6fc12adeeba46933871f94b22112f
Minh Pham <minhpt@crystal-techs.com>
2017-09-25 10:53:28 +0700

    add login

--- Modules/Frontend/Resources/views/login/index.blade.php
+++ Modules/Frontend/Resources/views/login/index.blade.php
@@ -3,10 +3,10 @@
     Login
 @endsection
 @section('content')
-
     <div class="page-content">
+
         <div class="row">
-            <div class="col-xs-12 col-md-5 col-md-offset-3">
+            <div class="col-md-5 col-md-offset-3">
                 <div class="widget-box">
                     <div class="widget-header widget-header-blue widget-header-flat">
                         <h4 class="widget-title lighter">Login</h4>
@@ -14,20 +14,50 @@
 
                     <div class="widget-body">
                         <div class="widget-main">
-                            <div class="form-group">
-                                <label class="bolder blue control-label col-sm-3 col-md-2" for="display_name">Username</label>
-                                <div class="col-sm-9 col-md-6">
-                                    <div class="clearfix">
-                                        <input class="form-control" id="display_name" name="display_name">
-                                        @if($errors->has('display_name'))
-                                            <p style="color:red">{!! $errors->first('display_name') !!}</p>
-                                        @endif
+                            <form method="post" action="{{route('role.create')}}"
+                                  class="form-horizontal validation-form">
+                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                <div class="form-group">
+                                    <label class="bolder control-label col-sm-3 col-md-2"
+                                           for="name">Username:</label>
+                                    <div class="col-sm-9 col-md-8">
+                                        <div class="clearfix">
+                                            <input name="username" id="username" class="form-control">
+                                            @if($errors->has('username'))
+                                                <p style="color:red">{!! $errors->first('username') !!}</p>
+                                            @endif
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
+                                <div class="form-group">
+                                    <label class="bolder control-label col-sm-3 col-md-2"
+                                           for="name">Password:</label>
+                                    <div class="col-sm-9 col-md-8">
+                                        <div class="clearfix">
+                                            <input name="password" id="password" class="form-control">
+                                            @if($errors->has('password'))
+                                                <p style="color:red">{!! $errors->first('password') !!}</p>
+                                            @endif
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="bolder blue control-label col-sm-3 col-md-2"
+                                           for="name"></label>
+                                    <div class="col-sm-9 col-md-8">
+                                        <div class="clearfix">
+                                            <button type="submit" class="btn btn-primary">Login</button>
+                                            <a href="/" type="button" class="btn btn-warning">Forgot Password</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
                     </div>
+                </div>
             </div>
         </div>
+
+
     </div>
 @endsection
